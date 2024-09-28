595cc1e2958d4423196c369317d2be9f5111e192
Endekalu Simon <endekalu.simon.haile@gmail.com>
2024-09-28 15:09:18 +0300

    Implement ZipDataIngestor for CSV extraction and add .gitignore file
    - Created an abstract base class `DataIngestor` for defining the data ingestion interface.
    - Implemented `ZipDataIngestor` for extracting CSV files from .zip archives and loading them into pandas DataFrames.
    - Added error handling for non-zip files, missing CSV files, and multiple CSV files in the archive.
    - Introduced `DataIngestorFactory` to dynamically return the appropriate ingestor based on file extension.
    - Added a `.gitignore` file to exclude unnecessary files from version control.

---
+++ src/ingest_data.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import zipfile
+from abc import ABC, abstractmethod
+
+# Define an abstract class for data Ingestor
+class DataIngestor(ABC):
+    @abstractmethod
+    def ingest(self, file_path:str) -> pd.Dataframe:
+        """Abstract method to ingest data from a given file."""
+        pass
+
+# Implement a concrete class for ZIP ingestion
+class ZipDataIngestor(DataIngestor):
+    def ingest(self, file_path: str) -> pd.Dataframe:
+        if not file_path.endswith(".zip"):
+            raise ValueError("The provided file is not a .zip file")
+        
+        # Ensure the file is a zip file
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            zip_ref.extractall("extracted_data")
+
+        # Extract the zip file
+        extracted_files = os.listdir("extracted_data")
+        csv_files = [f for f in extracted_files if f.endswith(".csv")]
+
+        if len(csv_files) == 0:
+            raise FileNotFoundError("No csv file found in the extracted data.")
+        if len(csv_files) > 1:
+            raise ValueError("Multiple CSV files found. Please specify which one to use.") 
+
+        # Read the CSV into a Dataframe
+        csv_file_path = os.path.join("extracted_data", csv_files)
+        df = pd.read_csv(csv_file_path)   
+        return df    
+
+class DataInestorFactory:
+    @staticmethod
+    def get_data_ingestor(file_extension:str) -> DataIngestor:
+        if file_extension == ".zip":
+            return ZipDataIngestor()
+        else:
+            raise ValueError(f"No ingestor available for file extension: {file_extension}")
+
